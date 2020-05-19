e560a43b00931a105f457123eba8f149341ec30b
tujunyu@rocky.games <tjy940525>
2020-05-19 19:30:17 +0800

    完成对角线遍历：这个是看了别人的博客才写出来的，最开始没有很认真的去总结规律，看完别人的思路才知道 原来可以如此简单，哈哈

--- 1.关于数组的学习/4.对角线遍历/Program.cs
+++ 1.关于数组的学习/4.对角线遍历/Program.cs
@@ -15,6 +15,13 @@ using System.Collections;
 
 说明:
 给定矩阵中的元素总数不会超过 100000 。
+
+分析：实际上是找规律 对规律的不同情况进行处理
+连线：分为右上和左下
+右上：最右边：下一个点  正下方
+      最上边：下一个点  正右方
+左下：最左边：下一个点  正下方
+      最下边：下一个点  正右方
 */
 namespace _4.对角线遍历
 {
@@ -28,71 +35,77 @@ namespace _4.对角线遍历
                 new int[]{ 7, 8, 9 }
             };
             Program p = new Program();
-            ArrayList numArray = p.FindDiagonalOrder(matrix);
-            for (int i = 0; i < numArray.Count; i++)
+            int[] numArray = p.FindDiagonalOrder(matrix);
+            for (int i = 0; i < numArray.Length; i++)
             {
                 Console.Write(numArray[i] + "-");
             }
         }
 
-        public ArrayList FindDiagonalOrder(int[][] matrix)
+        public int[] FindDiagonalOrder(int[][] matrix)
         {
-            if (matrix.Length == 0)
+            //如果是空数组直接返回一个空数组
+            if (matrix.Length == 0 || matrix == null)
             {
-                return new ArrayList(0);
+                return new int[] { };
             }
-            int row = matrix.Length;
-            int col = matrix[0].Length;
-            ArrayList numArray = new ArrayList(); //创建一个满足空间的 数组
-            //对角线的数量 是row+col-1
-            //对角线diagonal 单数条 1 3 5...都是朝向右上角    双数条 2 4 6...都是朝向左下角
-            //当diagonal<=col时,始终从0开始；
-            //当diagonal>col时，每当diagonal+1，起始row也+1 也就是diagonal-col;
-            //当diagonal=row时，应该就是跑到最后一个数了，row-1
-            int diagonal = row + col - 1; //得到对角线
-            int rowStart = 0;//开始的行下标
-            int rowEnd = 0;  //结束的行下标
-            for (int i = 1; i <= diagonal; i++)
+
+            int m = matrix.Length;     //行数
+            int n = matrix[0].Length;  //列数
+            int[] ans = new int[m * n];//新建数组，大小为二维数组元素个数
+            int Flag = 0;//flag是标志位，0代表右上，1代表左下
+            //a、b是当前遍历点的行列坐标，a为行，b为列
+            int rowIndex = 0; 
+            int colIndex = 0;
+
+            for (int i = 0; i < m * n; i++)
             {
-                //首先获取当前应该遍历的下标数据
-                if (i <= col)
+                //每一次遍历先保存该点的数值
+                ans[i] = matrix[rowIndex][colIndex];
+                //遍历到了最后一个点，就退出循环
+                if (rowIndex == m - 1 && colIndex == n - 1)
                 {
-                    rowStart = 0;
-                }
-                else
-                {
-                    rowStart = diagonal - col;
+                    break;
                 }
 
-                //也要判断下是不是到最后一个数了
-                if (i >= row)
-                {
-                    rowEnd = row - 1;
-                }
-                else
-                {
-                    rowEnd = i;
-                }
-
-                //单数是右上  双数是左下
-                if (i % 2 == 1)
-                {
-                    for (int j = rowEnd; j > rowStart; j--)
-                    {
-                        numArray.Add(matrix[j][i - j]);
+                if (Flag == 0)
+                {//方向是右上
+                    if (colIndex == n - 1)
+                    {//当前遍历点在右边界上
+                        ++rowIndex;
+                        Flag = 1;
+                    }
+                    else if (rowIndex == 0)
+                    {//当前点在上边界且不在右边界上
+                        ++colIndex;
+                        Flag = 1;
+                    }
+                    else
+                    {//当前点不在边界上
+                        --rowIndex;
+                        ++colIndex;
                     }
                 }
                 else
-                {
-                    for (int j = rowStart; j < rowEnd; j++)
-                    {
-                        numArray.Add(matrix[j][i - j]);
+                {//方向是左下
+                    if (rowIndex == m - 1)
+                    {//当前点在下边界上
+                        ++colIndex;
+                        Flag = 0;
+                    }
+                    else if (colIndex == 0)
+                    {//当前点在左边界上且不在下边界上
+                        ++rowIndex;
+                        Flag = 0;
+                    }
+                    else
+                    {//当前点不在边界上
+                        ++rowIndex;
+                        --colIndex;
                     }
                 }
-
             }
-
-            return numArray;
+            return ans;
         }
     }
 }
